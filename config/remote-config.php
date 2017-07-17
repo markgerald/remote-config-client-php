eb0d4080c4767c281619b9e14a6f71ebe9cdc985
flaviozantut <flaviozantut@gmail.com>
2017-07-17 10:38:50 -0300

    Use app env

--- config/remote-config.php
+++ config/remote-config.php
@@ -4,7 +4,7 @@ return [
     'username'  => env('REMOTE_CONFIG_USERNAME', ''),
     'password'  => env('REMOTE_CONFIG_PASSWORD', ''),
     'application' => env('REMOTE_CONFIG_APPLICATION', ''),
-    'environment' => env('REMOTE_CONFIG_ENVIRONMENT', 'development'),
+    'environment' => env('REMOTE_CONFIG_ENVIRONMENT', env('APP_ENV')),
     'cache-life-time' => env('REMOTE_CONFIG_CACHE_LIFE_TIME', 3600),
     'cache-directory' => env('REMOTE_CONFIG_CACHE_DIRECTORY'),
 ];
